8b070d06d144378b7f4e0e490d76b18acb10040e
YirenNing24 <paulanthonyarriola@gmail.com>
2024-12-10 18:56:35 +0800

    keydb typo

--- src/db/keydb.client.ts
+++ src/db/keydb.client.ts
@@ -1,7 +1,9 @@
 import { createClient } from 'redis';
 import { KEYDB_HOST, KEYDB_PORT, KEYDB_PASSWORD } from '../config/constants';
 
-const keydb = createClient({ url: `redis://:${KEYDB_PASSWORD}@${KEYDB_HOST}:${KEYDB_PORT}` });
+const keydbUrl = `redis://:${KEYDB_PASSWORD}@${KEYDB_HOST}:${KEYDB_PORT}`;
+
+const keydb = createClient({ url: keydbUrl });
 
 // Listen for error events
 keydb.on('error', err => console.log('KeyDB Client Error', err));
