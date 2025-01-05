f89df95705dece1288a00be085de6ab053b6a42a
YirenNing24 <paulanthonyarriola@gmail.com>
2025-01-05 09:31:08 +0800

    feat: update buyFromListing call to use buyer's wallet address for improved flexibility

--- src/game.services/store.services/store.service.ts
+++ src/game.services/store.services/store.service.ts
@@ -189,7 +189,7 @@ export default class StoreService {
       };
   
       // Call the buyFromListing function
-      await engine.marketplaceDirectListings.buyFromListing(CHAIN, CARD_MARKETPLACE, ENGINE_ADMIN_WALLET_ADDRESS, requestBody, false, "", buyerWalletAddress);
+      await engine.marketplaceDirectListings.buyFromListing(CHAIN, CARD_MARKETPLACE, buyerWalletAddress, requestBody, false, "", buyerWalletAddress);
 
     } catch(error: any) {
       console.log(error);
