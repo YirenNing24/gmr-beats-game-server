8e9942897880ea7371bec4388d092146895c8db9
YirenNing24 <paulanthonyarriola@gmail.com>
2025-04-08 18:19:44 +0800

    test

--- src/game.services/scores.services/scores.service.ts
+++ src/game.services/scores.services/scores.service.ts
@@ -107,7 +107,7 @@ class ScoreService {
 	 */
 	private async insertScoreToMongo(scoreWithRewards: any, username: string | null): Promise<void> {
 
-		console.log("monggis 1: ", scoreWithRewards)
+		console.log("monggis 3: ", scoreWithRewards)
 		let retries = 3;
 		while (retries > 0) {
 			let client: MongoClient | null = null;
